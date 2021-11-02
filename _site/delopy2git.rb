bb0e9d4d99206ad876f526677a0c08973a6aa305
Tracylmt <ml4683@columbia.edu>
2021-11-02 13:53:40 -0400

    initial github pages site

--- _site/delopy2git.rb
+++ _site/delopy2git.rb
@@ -1,11 +1,11 @@
 # puts `git init`
 # puts `cd webzero-test`
-# puts `git remote add origin https://github.com/Tracylmt/webzero-test.git`
+
 puts `cd output_local`
 puts `git checkout --orphan gh-pages`
 puts `bundle add webrick`
 puts `bundle exec jekyll build output_local`
 puts `git add .`
-puts `git commit -m ".gitignore added"`
-puts `git push origin main`
-puts `bundle exec jekyll serve --watch`
+puts `git commit -m "initial github pages site"`
+puts `git remote add origin https://github.com/Tracylmt/webzero-test.git`
+puts `git push -u origin main`
