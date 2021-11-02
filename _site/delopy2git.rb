0342ea8717654276cbdaf12110a10fb71a87b8d1
Tracylmt <ml4683@columbia.edu>
2021-11-02 13:44:21 -0400

    .gitignore added

---
+++ _site/delopy2git.rb
@@ -0,0 +1,10 @@
+# puts `git init`
+# puts `cd webzero-test`
+# puts `git remote add origin https://github.com/Tracylmt/webzero-test.git`
+puts `cd output_local`
+puts `git checkout --orphan gh-pages`
+puts `bundle add webrick`
+puts `bundle exec jekyll build output_local`
+puts `git add .`
+puts `git commit -m ".gitignore added"`
+puts `git push origin main`
