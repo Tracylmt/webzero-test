1fa73b417344e20b8236097d4f623607c13f269d
Tracylmt <ml4683@columbia.edu>
2021-11-02 20:46:11 -0400

    uploaded github pages site

--- delopy2git.rb
+++ delopy2git.rb
@@ -1,15 +1,16 @@
-# puts `git init`
+puts `git init`
 # puts `cd webzero-test`
 
-# puts Dir.pwd
+puts Dir.pwd
 
-puts `cd ./output_local`
-# puts `git checkout --orphan gh-pages`
-puts `bundle add webrick`
+puts `cd #{Dir.pwd}/output_local/`
+# puts `bundle add jekyll`
+# puts `bundle add webrick --1.7`
+puts `git checkout gh-pages`
 puts `bundle exec jekyll build output_local`
 # puts `bundle exec jekyll serve --watch`
 puts `git add .`
-puts `git commit -m "initial github pages site"`
-# puts `git remote add origin https://github.com/Tracylmt/webzero-test.git`
-puts `git push origin main`
-puts `git remote prune origin`
+puts `git commit -m "uploaded github pages site"`
+puts `git remote add origin https://github.com/Tracylmt/webzero-test.git`
+# # puts `git clone add origin https://github.com/ESaaS-4995/WebZero-Resume.git`
+puts `git push origin gh-pages`
